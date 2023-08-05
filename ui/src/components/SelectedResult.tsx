38326e6cb3935582b2b0eb9750fc458834cad018
leonid zharikov <leonid.zharikov@quadcode.com>
2023-08-05 14:15:26 +0300

    add count jumps info

--- ui/src/components/SelectedResult.tsx
+++ ui/src/components/SelectedResult.tsx
@@ -10,15 +10,28 @@ const SelectedResult: React.FC<SelectedResultProps> = ({selectedResult}) => {
     const route: Waypoint[] = Array.from(find_where_to_sell_blue(selectedResult.id, 0))
         .map(ssid => get_waypoint(ssid)!);
 
+    const jumps = (route: Waypoint[]): number => {
+        if (route.length == 0) {
+            return 0
+        }
+
+        return route.length - 1
+    }
+
     return (
-        <div>
+        <div className="w-[500px]">
             <h2 className="text-white">Кратчайший маршрут</h2>
-            <p className="text-white">От: {selectedResult.name}</p>
+            <div className="flex">
+                <p className="text-white">От: {selectedResult.name}</p>
+                <p className="text-white ml-auto">Jumps: <strong>{jumps(route)}</strong></p>
+            </div>
             <ul>
                 {route.map((waypoint) => (
                     <li key={waypoint.id}
                         className="cursor-pointer hover:font-bold"
-                        onClick={() => {navigator.clipboard.writeText(waypoint.name)}}>
+                        onClick={() => {
+                            navigator.clipboard.writeText(waypoint.name)
+                        }}>
                         <span className={securityColor(waypoint.security)}>{waypoint.name}</span>
                     </li>
                 ))}
